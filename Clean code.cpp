6dadb938dc6c1ba2633449ad76edb29fe487bb40
ankolodz <49743845+ankolodz@users.noreply.github.com>
2019-05-05 23:29:56 +0200

    Update Clean code.cpp

--- Clean code.cpp
+++ Clean code.cpp
@@ -8,30 +8,29 @@
 
 using namespace std;
 
-int cutInt (string input, int start, int length){
-    string tmp =input.substr(start,length);
-    return atoi(tmp.c_str());
+int stringPartToInt (string input, int start, int length){
+    input =input.substr(start,length);
+    return atoi(input.c_str());
 }
 vector<tm> parse (string input){
     vector<tm> endVector;
     input = input.substr (1,input.length()-2);
     tm parsedDate;
-    char *singleDate;
     const char* inputChar = input.c_str();
     char * date = strdup (inputChar);
-    singleDate = strtok (date,",");
+    char *singleDate = strtok (date,",");    
     while (singleDate!=NULL){
         string dateAfter = singleDate;
         if (dateAfter.length()!=21){
-            cout<<"Incorect input: "<<dateAfter<<endl;
+            cout<<"Incorect input\n";
             exit (-1);
         }
-    parsedDate.tm_year=cutInt (dateAfter,1,4);
-    parsedDate.tm_mon=cutInt (dateAfter,6,2);
-    parsedDate.tm_mday=cutInt (dateAfter,9,2);
-    parsedDate.tm_hour=cutInt (dateAfter,12,2);
-    parsedDate.tm_min=cutInt (dateAfter,15,2);
-    parsedDate.tm_sec=cutInt (dateAfter,18,2);
+    parsedDate.tm_year=stringPartToInt (dateAfter,1,4);
+    parsedDate.tm_mon=stringPartToInt (dateAfter,6,2);
+    parsedDate.tm_mday=stringPartToInt (dateAfter,9,2);
+    parsedDate.tm_hour=stringPartToInt (dateAfter,12,2);
+    parsedDate.tm_min=stringPartToInt (dateAfter,15,2);
+    parsedDate.tm_sec=stringPartToInt (dateAfter,18,2);
     endVector.push_back(parsedDate);
     singleDate = strtok (NULL,",");     
     }
@@ -40,20 +39,18 @@ vector<tm> parse (string input){
     return endVector; 
     
 }
-bool isNewSesion (tm lastLog, tm currentLog){
+bool isNewSession (tm lastLog, tm currentLog){
     int timeDiff = mktime(&lastLog)-mktime(&currentLog);
     return !(timeDiff <= 30*60);
 }
 bool isNewDay (tm lastLog, tm currentLog){
-    if (currentLog.tm_year==lastLog.tm_year && 
+    return !(currentLog.tm_year==lastLog.tm_year && 
         currentLog.tm_mon==lastLog.tm_mon &&
         currentLog.tm_mday ==lastLog.tm_mday)
-            return false;
-    return true;
 }
 bool isLessThanTreeDays (tm lastLog, tm currentLog){
     int timeDiff = mktime(&currentLog)-mktime(&lastLog);
-    return (timeDiff<=THREE_DAYS);
+    return timeDiff<=THREE_DAYS;
 
 }
 bool sendRequestToClient (string input){
@@ -65,7 +62,7 @@ bool sendRequestToClient (string input){
     lastLog = datesVector[size];
     currentLog = datesVector[size];
     while (size>=0 && isLessThanTreeDays(lastLog,currentLog)){
-        if (isNewSesion(lastLog,currentLog))
+        if (isNewSession(lastLog,currentLog))
             sessionLog++;
         if (isNewDay(lastLog,currentLog))
             days_log++;
@@ -74,7 +71,6 @@ bool sendRequestToClient (string input){
         currentLog = datesVector[size];        
     }
     return sessionLog>=6 && days_log==3;
-
 }
 int main(){
     string input ="['2017-03-10 08:13:11','2017-03-10 19:01:27','2017-03-11 07:35:55','2017-03-11 16:15:11','2017-03-12 08:01:41','2017-03-12 17:19:08']";
